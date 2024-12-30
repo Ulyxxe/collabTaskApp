ae11e02c94cc19f7acf36d6e0f131c4da756ec2f
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 11:11:41 +0100

    Employee, Project update and Task creation

--- src/main/java/org/ulysse/collabtaskapp/Project.java
+++ src/main/java/org/ulysse/collabtaskapp/Project.java
@@ -1,4 +1,14 @@
 package org.ulysse.collabtaskapp;
 
+import javafx.concurrent.Task;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Project {
+    private int id;
+    private String name;
+    private LocalDate deadline;
+    private List<T> tasks;
+    private List<Employee> members;
 }
