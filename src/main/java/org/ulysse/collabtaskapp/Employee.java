c70ef1271d1177bf8cc5a92e4d9678aab7b5da1b
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 10:32:48 +0100

    Update Employee.java

--- src/main/java/org/ulysse/collabtaskapp/Employee.java
+++ src/main/java/org/ulysse/collabtaskapp/Employee.java
@@ -14,4 +14,30 @@ public class Employee {
         this.projectHistory = new ArrayList<>();
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Project> getProjectHistory() {
+        return projectHistory;
+    }
+    public void addProjectToHistory(Project project) {
+        if (!projectHistory.contains(project)) {
+            projectHistory.add(project);
+        }
+    }
+
 }
