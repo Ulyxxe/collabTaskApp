9e2e1c51a7efff10d49a3f88a590a3deb5db6e9e
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 12:00:09 +0100

    Update Task.java

--- src/main/java/org/ulysse/collabtaskapp/Task.java
+++ src/main/java/org/ulysse/collabtaskapp/Task.java
@@ -2,6 +2,7 @@ package org.ulysse.collabtaskapp;
 
 import javafx.scene.layout.Priority;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task {
@@ -12,4 +13,13 @@ public class Task {
     private Status status;
     private Priority priority;
     private List<Employee> assignedMembers;
+
+    public Task(int id, String title, String description, Status status, Priority priority){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.assignedMembers = new ArrayList<>();
+    }
 }
