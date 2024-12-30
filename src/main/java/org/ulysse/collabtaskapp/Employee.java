7d4dcbff233b09b29613d9d7a8be3177ed4ce1d4
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 10:11:43 +0100

    Update Employee.java

--- src/main/java/org/ulysse/collabtaskapp/Employee.java
+++ src/main/java/org/ulysse/collabtaskapp/Employee.java
@@ -1,4 +1,17 @@
 package org.ulysse.collabtaskapp;
 
+import java.util.ArrayList;
+
 public class Employee {
+
+    private int id;
+    private String role;
+    private List<Project> projectHistory;
+
+    public Employee (int id, String role){
+        this.id = id;
+        this.role = role;
+        this.projectHistory = new ArrayList<>();
+
+    }
 }
