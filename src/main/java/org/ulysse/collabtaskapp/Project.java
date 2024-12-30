14a33b7a3835783ef93e0eded05539fcb4d68336
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 11:38:55 +0100

    Update Project.java

--- src/main/java/org/ulysse/collabtaskapp/Project.java
+++ src/main/java/org/ulysse/collabtaskapp/Project.java
@@ -1,14 +1,75 @@
 package org.ulysse.collabtaskapp;
 
-import javafx.concurrent.Task;
-
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Project {
     private int id;
     private String name;
     private LocalDate deadline;
-    private List<T> tasks;
+    private List<Task> tasks;
     private List<Employee> members;
+
+    public Project (int id, String name, LocalDate deadline){
+        this.id = id;
+        this.name = name;
+        this.deadline = deadline;
+        this.tasks = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public List<Employee> getMembers() {
+        return members;
+    }
+
+    public void addTask(Task task) {
+        if (!tasks.contains(task)) {
+            tasks.add(task);
+        }
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public void addMember(Employee employee) {
+        if (!members.contains(employee)) {
+            members.add(employee);
+        }
+    }
+
+    public void removeMember(Employee employee) {
+        members.remove(employee);
+    }
+
+
 }
