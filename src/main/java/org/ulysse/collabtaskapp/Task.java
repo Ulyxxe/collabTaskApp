1957d6a50743667f2a3afef5a39f8ba4d5ab96e3
Clemcloum1904 <c.durye@gmail.com>
2024-12-30 11:52:49 +0100

    Update Task.java

--- src/main/java/org/ulysse/collabtaskapp/Task.java
+++ src/main/java/org/ulysse/collabtaskapp/Task.java
@@ -1,4 +1,15 @@
 package org.ulysse.collabtaskapp;
 
+import javafx.scene.layout.Priority;
+
+import java.util.List;
+
 public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+    private Status status;
+    private Priority priority;
+    private List<Employee> assignedMembers;
 }
