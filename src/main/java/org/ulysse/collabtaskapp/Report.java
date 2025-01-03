0da0e2e122786c749eb7b9d03280dc07fbd2e2c8
Clemcloum1904 <c.durye@gmail.com>
2025-01-03 10:22:51 +0100

    Update Report.java

--- src/main/java/org/ulysse/collabtaskapp/Report.java
+++ src/main/java/org/ulysse/collabtaskapp/Report.java
@@ -1,5 +1,38 @@
 package org.ulysse.collabtaskapp;
 
 public class Report {
-    
+    private int id;
+    private String type;
+    private String format;
+
+    public Report (int id, String type, String format){
+        this.id = id;
+        this.type = type;
+        this.format = format;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
 }
