a2bc6afd18335442908b08027b70f359a187161d
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2025-01-14 15:39:15 +0100

    add a name to employeeÃ

--- src/main/java/org/ulysse/collabtaskapp/Employee.java
+++ src/main/java/org/ulysse/collabtaskapp/Employee.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class Employee {
 
     private int id;
+    private String name;
     private String role;
     private List<Project> projectHistory;
 
-    public Employee (int id, String role){
+    public Employee (int id, String name, String role){
         this.id = id;
+        this.name = name;
         this.role = role;
         this.projectHistory = new ArrayList<>();
 
