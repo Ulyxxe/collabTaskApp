0bc5c80de942347f3af323758565bf8bc618e2c3
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2025-01-14 14:49:45 +0100

    add method to add todo task

--- src/main/java/org/ulysse/collabtaskapp/KanbanBoard.java
+++ src/main/java/org/ulysse/collabtaskapp/KanbanBoard.java
@@ -1,14 +1,19 @@
 package org.ulysse.collabtaskapp;
 import java.util.ArrayList;
 import java.util.List;
+
 public class KanbanBoard {
-    private List<String> columns;
-    private List<Task> tasks;
+    private List<Task> toDo;
+    private List<Task> inProgress;
+    private List<Task> done;
 
     public KanbanBoard() {}
 
-    public List<String> getColumns() {
-        return columns;
+    public List<String> addTaskToDo(Task task) {
+        if (task != null) {
+            toDo.add(task);
+            System.out.println("Task added: " + task.getTitle());
+        };
     }
 
 }
