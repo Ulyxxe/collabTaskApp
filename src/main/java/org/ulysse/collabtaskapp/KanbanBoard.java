75cde555c09a1e3925fa5dc0b0ceea1ab7dc3d6d
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2025-01-14 14:53:36 +0100

    move a task and assign column to a task

--- src/main/java/org/ulysse/collabtaskapp/KanbanBoard.java
+++ src/main/java/org/ulysse/collabtaskapp/KanbanBoard.java
@@ -16,5 +16,38 @@ public class KanbanBoard {
         };
     }
 
+    public boolean moveTask(Task task, String targetColumn) {
+        if (task != null || targetColumn == null) {
+            return false;
+        }
+        if (toDo.remove(task)){
+            addToColumn(task, targetColumn);
+            return true;
+        } else if (inProgress.remove(task)) {
+            addToColumn(task, targetColumn);
+            return true;
+        } else if (done.remove(task)) {
+            addToColumn(task, targetColumn);
+            return true;
+        }
+        return false;
+    }
+
+    private void addToColumn(Task task, String targetColumn) {
+        switch (targetColumn.toLowerCase()) {
+            case "todo":
+                toDo.add(task);
+                break;
+            case "inprogress":
+                inProgress.add(task);
+                break;
+            case "done":
+                done.add(task);
+                break;
+            default:
+                System.out.println("Invalid column specified");
+        }
+    }
+
 }
 
